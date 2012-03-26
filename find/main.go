0c5a60ba094bc9c246d3a6c5b51cf42e4794c3d3
str1ngs <mike.rosset@gmail.com>
2012-03-26 11:09:53 -0700

    simplify code for now

--- find/main.go
+++ find/main.go
@@ -6,35 +6,19 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"time"
 )
 
 var (
 	debug = flag.Bool("d", false, "output debugging details")
-	usage = `Usage: find path [arguements]
-`
 )
 
-var Usage = func() {
-	fmt.Fprintf(os.Stderr, usage)
-	flag.PrintDefaults()
-}
-
 func main() {
-	start := time.Now()
-	defer func() {
-		if *debug {
-			fmt.Println(os.Args[0], "done in", time.Now().Sub(start))
-		}
-	}()
-	flag.Usage = Usage
 	flag.Parse()
-	if len(flag.Args()) == 0 {
-		flag.Usage()
-		os.Exit(1)
+	args := flag.Args()
+	if flag.NArg() == 0 {
+		args = []string{"."}
 	}
-	path := flag.Arg(0)
-	if err := filepath.Walk(path, walkFunc); err != nil {
+	if err := filepath.Walk(args[0], walkFunc); err != nil {
 		log.Fatal(err)
 	}
 }
